7fc82d90fe1769eb382f4e65b00de10f8ce7e323
Luiz Sampaio <sampaio.ime@gmail.com>
2020-06-23 02:37:53 -0300

    adding and listing school - feature now working

--- app.py
+++ app.py
@@ -3,13 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schools.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class School(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    name = db.Column(db.String(200), nullable = False)
+    neigh = db.Column(db.String(200), nullable = False)
+    students = db.Column(db.Integer, default = 0)
+    endow = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+class Classes(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    students = db.Column(db.Integer, default = 0)
+    school = db.Column(db.String(200), nullable = False)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -17,19 +28,22 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content = task_content)
+        name = request.form['nome']
+        bairro = request.form['bairro']
+        verba = request.form['verba']
+
+        new_school = School(name = name, neigh = bairro, endow = verba)
 
         try:
-            db.session.add(new_task)
+            db.session.add(new_school)
             db.session.commit()
             return redirect('/')
         except:
             return "There was an issue"
 
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks = tasks)
+        schools = School.query.order_by(School.id).all()
+        return render_template('index.html', schools = schools)
 
 @app.route('/delete/<int:id>')
 def delete(id):
